8ec3fd10bbb42b0c3b18d5483f40309358861f11
marcelo-alves87 <marceloalves87@gmail.com>
2018-08-29 23:08:09 -0300

    Init

---
+++ PyVisa/Plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import csv
+
+style.use('fivethirtyeight')
+
+def input_file_csv(type1):
+    xs, ys = normalize_csv(type1 + '.csv')
+    plt.ylabel(type1)
+    plt.plot(xs,ys)
+
+def normalize_csv(filename):
+    xs = []
+    ys = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            s = row[0]
+            if(s.find('!') < 0 and s.find('BEGIN') < 0 and s.find('END') < 0):
+                xs.append(row[0])
+                ys.append(row[1])
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+     
+#input_file_csv('MLOGarithmic')
+input_file_csv('PHASe')
+#input_file_csv('SWR')
+
+
+plt.xlabel('Frequencies')
+plt.show()
+
