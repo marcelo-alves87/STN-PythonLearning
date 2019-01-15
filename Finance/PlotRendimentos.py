b740e667edefa3856a8ff87d8cb3398d3cdaa571
marcelo-alves87 <marceloalves87@gmail.com>
2019-01-14 23:10:07 -0300

    Init

--- Finance/PlotRendimentos.py
+++ Finance/PlotRendimentos.py
@@ -5,34 +5,30 @@ import numpy as np
 style.use('ggplot')
 
 #Valor Inicial
-P=100
+P=1000
 #Aporte Mensal (Meta)
-M=500
+M=100
 #Aporte Mensal (Real)
 MM=[]
 #Taxa de Juros Mensal (Meta) (%)
-i=9
+i=8
 #Taxa de Juros Mensal (Real) (%)
 ii=[]
 #Prazo (Meses, Começa em 1)
 n=60
 
-
-def formula_juros(exponencial,juros):
-    return (1 + juros/100)**exponencial
-
 def plotar_grafico_por_meses(aportes, juros, periodo=n, rendimentos=0):
     if periodo == 0:
         text = ('Total Poupado R$ {}\nJuros Recebidos R$ {}'
-                        + '\nMontante Final R$ {}\n\n\n\n').format(P + sum(aportes),rendimentos - sum(aportes),P + rendimentos)
-        plt.text(0.02, 0.5,text)
+                        + '\nMontante Final R$ {}\n\n\n\n').format(P + sum(aportes),rendimentos - sum(aportes) - P,rendimentos)
         print(text)
+        plt.text(0.02, 0.5,text)
     else:
         if periodo == n:
-            rendimentos += P*formula_juros(n-1,juros[n-1]) + aportes[0]
+            rendimentos = (P + aportes[0]) + (P + aportes[0])*juros[0]/100
         else:
-            rendimentos += aportes[periodo]*formula_juros(periodo,juros[periodo])
-        plt.scatter(n-periodo,rendimentos)
+            rendimentos = (rendimentos + aportes[periodo]) + (rendimentos + aportes[periodo])*juros[periodo]/100            
+        plt.scatter(n-periodo+1,rendimentos)
         plotar_grafico_por_meses(aportes,juros,periodo-1,rendimentos)
       
 def plotar_grafico():
