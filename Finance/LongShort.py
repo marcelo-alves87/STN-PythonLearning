02cac22148f222a872875ca47fa8be3747f3879c
marcelo-alves87 <marceloalves87@gmail.com>
2021-11-10 20:11:32 -0300

    Update

--- Finance/LongShort.py
+++ Finance/LongShort.py
@@ -85,33 +85,46 @@ def mean_diff(date):
     tickets = []
     for index, data in df_corr.iteritems():
         for index1, data1 in data.iteritems():
-           if data1 >= 0.96 and index != index1:
+           if data1 >= 0.95 and index != index1:
                try: 
                    df1 = pd.read_csv('stock_dfs/' + index + '.csv', parse_dates=True, index_col=0)
                    df1 = df1['Adj Close']
+
+                   df11 = pd.read_csv('stock_dfs/' + index + '.csv', parse_dates=True, index_col=0)
+                   df11 = df11['Volume']
+                   df11_value = round(df11[date_str] / 10**6,3)
+                  
                    df2 = pd.read_csv('stock_dfs/' + index1 + '.csv', parse_dates=True, index_col=0)
                    df2 = df2['Adj Close']
+
+                   df22 = pd.read_csv('stock_dfs/' + index1 + '.csv', parse_dates=True, index_col=0)
+                   df22 = df22['Volume'] 
+                   df22_value = round(df22[date_str] / 10**6,3)
+                   
                    value1 = df1[date_str]
-                   value2 = df2[date_str]                   
-                   if value1 > value2:
+                   value2 = df2[date_str]
+                   
+                   if value1 > value2 and df11_value > 1 and df22_value > 1:
                        df1 = df1.divide(df2)
                        df1 = df1.subtract(1)
                        value1 = df1[date_str]
                        mean10 = df1.rolling(window=10, min_periods=0).mean()
                        mean10_value = mean10[date_str]
-                       tickets.append({'date' : date_str, 'ticket1' :index, 'ticket2' :index1, 'corr' :data1, 'diff' : value1 - mean10_value})                                          
+                       diff = round(abs(value1 - mean10_value),3)
+                       
+                       tickets.append({'date' : date_str, 'ticket1' :index, 'ticket2' :index1, 'corr' : round(data1,3), 'diff' : diff, 'vol1' : df11_value , 'vol2' : df22_value})                                          
                except:
                    pass
                
     return tickets
 
-tickets = mean_diff(datetime.datetime(2021, 11, 9))
+tickets = mean_diff(datetime.datetime(2021, 11, 10))
 tickets.sort(reverse=True, key=sort_)
 #tickets = tickets[:100]
 #tickets.sort(reverse=True, key=sort2_)
 for ticket in tickets:
-    print(('Data: {} : {} e {};  Fator de correlação: {}, Diferença com média: {}').format(ticket['date'],ticket['ticket1'], ticket['ticket2'], ticket['corr'], ticket['diff']))
- 
-#plot('LREN3','AMAR3')
+    print(('Data: {} : {} e {} = volume ({} milhões e {} milhões);  Fator de correlação: {}, Diferença com média: {}').format(ticket['date'],ticket['ticket1'], ticket['ticket2'], ticket['vol1'], ticket['vol2'], ticket['corr'], ticket['diff']))
+    
+#plot('JSRE11','DMVF3')
 
             
