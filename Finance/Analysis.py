556b45c54240e05d42f4d735ea761dae936947b9
marcelo-alves87 <marceloalves87@gmail.com>
2022-01-20 12:35:18 -0300

    Updating

--- Finance/Analysis.py
+++ Finance/Analysis.py
@@ -6,7 +6,7 @@ import datetime as dt
 
 PICKLE_FILE = 'btc_tickers.plk'
 color = sys.stdout.shell
-periods = ['15min','13min','11min','9min','7min','5min','3min','1min']
+periods = ['15min','13min','11min','9min','7min','5min','3min','1min'] # deve ser em ordem decrescente
 
 def join_cells(x):    
     return ';'.join(x[x.notnull()].astype(str))
@@ -63,12 +63,15 @@ def try_to_get_df():
     except:
         return None
 
+def sort(x):
+    return x[-1]
+
 def analysis(df):
     data = []
     grouped_df = df.groupby(["Papel"]).agg(join_cells)
     for group in grouped_df.iterrows():
         
-        
+        lev = group[1]['Lev.'].split(';')[0]
         list1 = group[1]['Hora'].split(';')
         list2 = group[1]['Último'].split(';')
         df1 = {'Hora' : list1, 'Último' : list2}
@@ -78,7 +81,7 @@ def analysis(df):
         
         if len(df1) > 1:
             data1 = []
-            data1.append(group[0])
+            data1.append([group[0],lev])
             for period in periods:
                 
                 df_resampled = df1.resample(period).last()
@@ -97,20 +100,48 @@ def analysis(df):
                 data1.append([df_resampled.index[-1],value])     
             data.append(data1)
                 
-    color.write('\n',"TODO")    
+    color.write('\n',"TODO")
+
+    
     for group in data:
         
-        color.write(group[0] + ':' ,"TODO")        
+        score = 0
+        len_periods = len(periods) - 1
+
+        for i in range(len_periods):
+            if isinstance(group[i + 1][1], int):
+                group[i + 1].append('R')
+            elif not isinstance(group[i + 2][1], int):
+                if (group[i + 1][1] > 0 and group[i + 2][1] > 0) or (group[i + 1][1] < 0 and group[i + 2][1] < 0):
+                    score += 1
+                    group[i + 1].append('G')
+                elif (group[i + 1][1] > 0 and group[i + 2][1] < 0) or (group[i + 1][1] < 0 and group[i + 2][1] > 0):
+                    group[i + 1].append('R')
+        group.append(score)            
+
+    data.sort(key=sort, reverse=True)
+    
+    for group in data:                
+        color.write(group[0][0] + '(' + group[0][1] + ')' + ':' ,"TODO")        
         for i,period in enumerate(periods):
             color.write(' ' + period + '(' + timestamp_to_str(group[i + 1][0]) + ') ',"TODO")
             value = group[i + 1][1]
             value_str =str(round(abs(value),3))
+             
             if value > 0:
                 color.write(value_str,"STRING")    
             elif value < 0:
                 color.write(value_str,"COMMENT")
             else:
                 color.write(value_str,"KEYWORD")
+            
+            if len(group[i + 1]) > 2:
+                value = group[i + 1][2]
+                if value == 'G':
+                    color.write(' *',"STRING")    
+                elif value == 'R':
+                    color.write(' *',"COMMENT")
+                
         color.write('\n',"TODO") 
 
 while True:
