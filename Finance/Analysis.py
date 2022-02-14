fcbba64f303a2146a7cb46413287c8b42dcad9c5
marcelo-alves87 <marceloalves87@gmail.com>
2022-02-14 19:01:12 -0300

    Fixing

--- Finance/Analysis.py
+++ Finance/Analysis.py
@@ -28,18 +28,18 @@ df = utils.try_to_get_df()
 
 df.dropna(inplace=True)
 
-style  = mpf.make_mpf_style(base_mpf_style='charles',mavcolors=['blue','orange','red'])
+style  = mpf.make_mpf_style(base_mpf_style='yahoo',mavcolors=['blue','orange','red'],y_on_right=False)
 
 fig = mpf.figure(style=style)
 plt.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.95, 0.95)
 
-dimension = [26,7]
-positions = [[(1,45),(50,52)],[(5,49),(54,56)], [(64,108),(113,115)],[(68,112),(117,119)], [(127,171),(176,178)]]
+dimension = [28,8]
+positions = [[(1,60)],[(5,64)], [(81,140)],[(85,144)],[(163,222)]]
 axes = []
 for pos in positions:
     ax = fig.add_subplot(dimension[0],dimension[1],pos[0])
-    av = fig.add_subplot(dimension[0],dimension[1],pos[1], sharex=ax)
-    axes.append([ax,av])
+    #av = fig.add_subplot(dimension[0],dimension[1],pos[1], sharex=ax)
+    axes.append([ax])
 
 
 
@@ -47,16 +47,18 @@ date = dt.datetime.strptime('2022-02-10 16:45:00','%Y-%m-%d %H:%M:%S')
 
 
 def analysis(ival):
-##    new_date =  date + dt.timedelta(minutes=ival)
-##    df1 = df[df['Hora'] < new_date.strftime("%Y-%m-%d %H:%M:%S")]
-    df1 = utils.try_to_get_df()
+    new_date =  date + dt.timedelta(minutes=ival)
+    
+    df1 = df[df['Hora'] < new_date.strftime("%Y-%m-%d %H:%M:%S")]
+    #df1 = utils.try_to_get_df()
     df1.reset_index(inplace=True)
     df1['Volume'] = df1['Volume'].apply(utils.to_volume)
+    
     df1['Hora'] = df1['Hora'].apply(utils.convert_to_datetime)
     df1.set_index('Hora',inplace=True)
         
     df4 = df1.groupby([pd.Grouper(freq=PERIOD), 'Papel'])['Último'].agg([('open','first'),('high', 'max'),('low','min'),('close','last')])
-    df5 = df1.groupby([pd.Grouper(freq=PERIOD), 'Papel'])['Volume'].agg([('volume','last')])
+    df5 = df1.groupby([pd.Grouper(freq=PERIOD), 'Papel'])['Volume'].agg([('volume','mean')])
     df6 = df1.groupby([pd.Grouper(freq=PERIOD), 'Papel'])['Máximo'].agg([('max','last')])
     df7 = df1.groupby([pd.Grouper(freq=PERIOD), 'Papel'])['Mínimo'].agg([('min', 'last')])
                                                                         
@@ -67,11 +69,14 @@ def analysis(ival):
     for i,ticket in enumerate(tickets):
                
         df0 = df1.loc[pd.IndexSlice[:,ticket], :]
+        df0['volume'] = df0['volume'].diff()
+        df0.fillna(0,inplace=True)
         df0.reset_index('Papel',inplace=True)
+        
         ax = axes[i][0]
-        av = axes[i][1]
+        #av = axes[i][1]
         ax.clear()
-        av.clear()
+        #av.clear()
 
         df0 = df0.iloc[RESOLUTION:]
         
@@ -85,12 +90,13 @@ def analysis(ival):
         for f in FIBONACCI:
             hlines.append(diff*f/100 + minimum)
         
-        mpf.plot(df0,hlines=dict(hlines=hlines,colors=['g','r','b','c','k'],linestyle='--'),type='candle',ax=ax,mav=(5,8,13),axtitle=ticket,volume=av, ylabel='', ylabel_lower='',xrotation=0, datetime_format='%H:%M')
+        mpf.plot(df0,hlines=dict(hlines=hlines,colors=['darkgreen','orangered','indianred','saddlebrown','greenyellow','lightgreen'],linestyle='--',linewidths=(1.3)),type='candle',ax=ax,mav=(5,8,13),axtitle=ticket,ylabel='', ylabel_lower='',xrotation=0, datetime_format='%H:%M')
         
     
 
 
-ani = animation.FuncAnimation(fig, analysis)    
+ani = animation.FuncAnimation(fig, analysis)
+
 mpf.show()    
     
 
--- Finance/DayTrade.py
+++ Finance/DayTrade.py
@@ -13,7 +13,7 @@ import numpy as np
 
 LEVERAGE_FILE = 'Alavancagem_Rico.txt'
 LEVERAGE = [5,6] #5% to 6%
-BTC_FILE = 'BTC Rico 11-02-2022-CoA.pdf'
+BTC_FILE = 'BTC Rico 14-02-2022-R0E.pdf'
 FREE_FLOAT_FILE = 'Free-Float_1-2022.csv'
 PICKLE_FILE = 'btc_tickers.plk'
 URL = "https://rico.com.vc/arealogada/home-broker"
