fb6567210cbdc064d3220423fdddf7f748aa25a2
marcelo-alves87 <marceloalves87@gmail.com>
2022-03-25 16:12:41 -0300

    Updating

--- Finance/Price_Alert.py
+++ Finance/Price_Alert.py
@@ -9,16 +9,19 @@ import os
 import json
 from selenium.common.exceptions import WebDriverException
 import Utils as utils
+import sys
 
-PRICE_ALERT = 'Price_Alert.txt' # please write the price interval
+color = sys.stdout.shell
+PRICE_ALERT = 'Price_Alert.txt' # write the price interval
 URL = "https://rico.com.vc/arealogada/home-broker"
 
-def notify(ticket):
+def notify(ticket, time1):
     path1 = 'Utils/' + ticket + '.mp3'    
     utils.play(ticket,path1,'pt-br')
     path1 = 'Utils/Strike.mp3'
     utils.play('Strike',path1,'en-us')
-
+    color.write('(' + time1 + ') ** ' + ticket + ' **\n','KEYWORD')
+    
 def get_page_source(driver):
    try :
        return driver.page_source       
@@ -44,8 +47,8 @@ def scrap_rico():
         tables = soup.find_all('table', class_='nelo-table-group') 
         
         df = pd.read_html(str(tables[0]))[0]
-
-        df = df[['Ativo','Último']]
+        
+        df = df[['Ativo', 'Data/Hora','Último']]
 
         df['Último'] = df['Último']/100
         data = {}
@@ -56,8 +59,9 @@ def scrap_rico():
               df2 = df[df['Ativo'] == ticket]
               if not df2.empty:
                  value = df2.iloc[-1]['Último']
+                 time1 = df2.iloc[-1]['Data/Hora']
                  if value >= interval[0] or value <= interval[1]:
-                    notify(ticket)
+                    notify(ticket,time1)
                     data.pop(ticket, None)
                     with open(PRICE_ALERT, 'w') as f:
                        json.dump(data, f)                  
