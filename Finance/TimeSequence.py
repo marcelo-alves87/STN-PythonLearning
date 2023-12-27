ae553cf02985e84176ad874e9bfa44890085615d
marcelo-alves87 <marceloalves87@gmail.com>
2023-12-27 18:36:54 -0300

    DayTrade

--- Finance/TimeSequence.py
+++ Finance/TimeSequence.py
@@ -1,9 +1,15 @@
 import datetime as dt
 import pdb
 
+# 27000 7 horas e 30 minutos
+# 27900 7 horas e 45 minutos
 # 28200 7 horas e 50 minutos
 # 28500 7 horas e 55 minutos
 # 28800 8 horas
+
+start = '10:00'
+period = 15
+module = 28800
 
 def fibonnaci(x):
     if x == 0 or x == 1:
@@ -11,21 +17,35 @@ def fibonnaci(x):
     else:
         return fibonnaci(x - 1) + fibonnaci(x - 2)
 
-date = '15:35' 
-
-
-date = dt.datetime.strptime(date, '%H:%M')
-norm = dt.datetime.strptime('10:05', '%H:%M')
-
-for i in range(15):
-    j = fibonnaci(i)
-    date1 = date + dt.timedelta(minutes=5*j)
-    diff = (date1 - norm).seconds
-    mod = diff % 28200
+def calculate(date, j):
+    date = dt.datetime.strptime(date, '%H:%M')
+    norm = dt.datetime.strptime(start, '%H:%M')
+    date1 = date + dt.timedelta(minutes=period*j)
+    diff = (date1 - norm).seconds 
+    mod = diff % module
     date1 = norm + dt.timedelta(hours=mod // 3600)
     date1 += dt.timedelta(minutes=((mod/3600) % 1) * 60 )
-    print('{} -> {}'.format(j, date1.strftime('%H:%M')))
+    return date1
 
+def sequence(date, verbose=True):
+    dict = {}
+    for i in range(15):
+        j = fibonnaci(i)
+        date1 = calculate(date, j)
+        dict[j] = date1
+        if verbose:
+            print('{} -> {}'.format(j, date1.strftime('%H:%M')))
+    return dict
 
+def levels(date, index1):
+    dict = sequence(date, False)
+    LEVELS = [0, 0.236, 0.382, 0.5, 0.786, 1]
+    for i in LEVELS:
+        k = calculate(date, i*index1)
+        print('{} -> {}'.format(i, k.strftime('%H:%M')))
+
+date = '14:45'        
+#sequence(date)
+levels(date, 55)
 
  
