339d58d27357942fdfdb5f21f818205be969b287
marcelo-alves87 <marceloalves87@gmail.com>
2024-01-12 16:25:22 -0300

    DayTrade

--- Finance/Offline.py
+++ Finance/Offline.py
@@ -18,31 +18,14 @@ import matplotlib.animation as animation
 yfin.pdr_override()
 
 MAIN_DF_FILE = 'main_df.pickle'
-FIRST_EMA_LEN = 10
-SECOND_EMA_LEN = 30
-count_bull = 0
-count_bear = 0
-tickets_corr = []
-CORR_THRESHOLD = 10
 LEVELS = [0.236, 0.382, 0.5, 0.618, 0.786, 1]
 
-#fazer correlação com 30 min ou 15 min e verificar movimentos em 5-min
-# most effective 5min ma cross
-def verify_trends(main_df):    
-    if not main_df.empty:
-       df = main_df['Ativo'].drop_duplicates()
-       for index,row in df.items():
-           if row != 'IBOV':
-               df_ticket = main_df[main_df['Ativo'] == row]
-               df_ticket.sort_index(inplace=True)
-               strategy_candles(row, df_ticket)
-               #strategy_ma(row, df_ticket)
             
 def get_data_from_yahoo(ticket, actual_date):
    
    if not os.path.exists('stock_dfs'):
       os.makedirs('stock_dfs')
-   start_date = actual_date - dt.timedelta(days=55)   
+   start_date = actual_date - dt.timedelta(days=15)   
    end_date = actual_date + dt.timedelta(days=1)   
    # just in case your connection breaks, we'd like to save our progress!
    if not os.path.exists('stock_dfs/{}.csv'.format(ticket)):
@@ -64,12 +47,6 @@ def format_ticket(ticket):
    else:
       ticket = ticket[0]
    return ticket
-
-def has_great_volume(vol):
-    if vol >= 10**6:
-        return True
-    else:
-        return False
 
 def format_volume(vol):
     if vol / 10**9 >= 1:
@@ -100,55 +77,7 @@ def update(df):
    df4.sort_index(inplace=True)
    return df4      
 
-def strategy_ma(name, df_ticket):
-     global count_bull, count_bear
-  
-     df_ticket['EMA_1'] = df_ticket['Último'].ewm(span=FIRST_EMA_LEN, adjust=False).mean()
-     df_ticket['EMA_2'] = df_ticket['Último'].ewm(span=SECOND_EMA_LEN, adjust=False).mean()
 
-     df_ticket = df_ticket[df_ticket.index >= dt.datetime.strftime(df_ticket.index[-1] - dt.timedelta(days = 115),'%Y-%m-%d')]
-
-     if has_great_volume(df_ticket['Financeiro'][-1]): 
-         if df_ticket[df_ticket['EMA_1'] > df_ticket['EMA_2']].empty:
-             print(name, 'Bearish', format_volume(df_ticket['Financeiro'][-1]))
-             count_bear += 1
-         elif df_ticket[df_ticket['EMA_1'] < df_ticket['EMA_2']].empty:
-             print(name, 'Bullish', format_volume(df_ticket['Financeiro'][-1]))
-             count_bull += 1
-
-
-def strategy_candles(name, df_ticket):
-     global count_bull, count_bear
-
-     df_ticket = df_ticket[df_ticket.index >= dt.datetime.strftime(df_ticket.index[-1] - dt.timedelta(days = 8),'%Y-%m-%d')]
-     
-     if has_great_volume(df_ticket['Financeiro'][-1]):
-         if df_ticket[df_ticket['Abertura'] > df_ticket['Último']].empty:
-             tickets_corr.append(name)
-             print(name, 'Bullish', format_volume(df_ticket['Financeiro'][-1]))
-             count_bull += 1
-         elif df_ticket[df_ticket['Abertura'] < df_ticket['Último']].empty:
-             tickets_corr.append(name)
-             print(name, 'Bearish', format_volume(df_ticket['Financeiro'][-1]))
-             count_bear += 1
-
-def correlation(main_df):
-    main_df.reset_index(inplace=True)
-    df = pd.DataFrame(main_df['Data/Hora'])
-    df.drop_duplicates(inplace=True)
-    for i in range(len(tickets_corr)):
-        df1 = main_df[main_df['Ativo'] == tickets_corr[i]][['Data/Hora', 'Último']]
-        df1.rename(columns={'Último': tickets_corr[i]}, inplace=True)
-        df = df.merge(df1, on='Data/Hora', how='left')
-    df = df[df['Data/Hora'] > df['Data/Hora'].iloc[-7]]
-    df_corr = df.corr()
-    for index, data in df_corr.iteritems():
-        for index1, data1 in data.iteritems():
-            if abs(data1) >= 0.992 and index != index1:
-                print(('{} e {} fator de correlação: {}').format(index, index1, data1))
-
-    
-         
 def get_tickets():
    data = []
    #data.append('IBOV')
@@ -170,120 +99,7 @@ def fibonnaci(x):
     else:
         return fibonnaci(x - 1) + fibonnaci(x - 2)
 
-def process_data_corr(main_df, verbose=False):
-    data = {}
-    tickers = main_df['Ativo']
-    tickers.drop_duplicates(inplace=True)
-    
-    for i,ticker in tickers.items():
 
-        
-        
-        df1 = main_df[main_df['Ativo'] == ticker]
-        
-        
-        df1['EMA_1'] = df1['Último'].ewm(span=FIRST_EMA_LEN, adjust=False).mean()
-        df1['EMA_2'] = df1['Último'].ewm(span=SECOND_EMA_LEN, adjust=False).mean()
-
-        df1 = df1[df1.index > '2023-11-01']
-
-        if not df1.empty:
-            date1 = df1.index[0]
-            last_date = df1.index[-1]
-            
-            df2 = df1[df1.index > df1.index[-1].strftime('%Y-%m-%d')]
-            volume = (df2['Financeiro'] * df2['Último']).sum()
-
-            if has_great_volume(volume):
-                while date1 < last_date:
-                    
-                    date1_str = dt.datetime.strftime(date1,'%Y-%m-%d')
-
-                    df2 = df1[df1.index > date1_str]
-                    
-                    df2 = df2[df2.index <  dt.datetime.strptime(\
-                        date1_str, '%Y-%m-%d') + dt.timedelta(days = 1)]
-
-                    if not df2.empty:
-                        df3 = df2[df2['EMA_1'] > df2['EMA_2']]
-                        df4 = df2[df2['EMA_1'] < df2['EMA_2']]
-                        if len(df3) <= CORR_THRESHOLD:
-                            if date1_str in data:
-                                data[date1_str]['Bearish'].append(ticker) 
-                            else:
-                                data[date1_str] = {'Bearish' : [ticker], 'Bullish' : []}                         
-                        elif len(df4) <= CORR_THRESHOLD:
-                            if date1_str in data:
-                                data[date1_str]['Bullish'].append(ticker) 
-                            else:
-                                data[date1_str] = {'Bullish' : [ticker], 'Bearish' : []}        
-                            
-                    date1 += dt.timedelta(days = 1)
-    df = pd.DataFrame(data)
-    df = df.transpose()
-    df.sort_index(inplace=True)
-    
-    if verbose:
-        for i,row in df.iterrows():
-            print('----------------')
-            print(i)
-            print('----------------')
-            print('Bullish:')
-            print(row['Bullish'])
-            print('----------------')
-            print('Bearish:')
-            print(row['Bearish'])
-            print('----------------')
-    return df
-
-def process_hit_corr(hit, tickets, df):
-    for k,row in df.iterrows():
-        for i in range(len(tickets)):
-            for j in range(len(tickets)):
-                if i > j:
-                    index = tickets[i] + ' ' + tickets[j]
-                    if (tickets[i] in row['Bearish'] and tickets[j] in row['Bearish'] ) or\
-                        (tickets[i] in row['Bullish'] and tickets[j] in row['Bullish']):
-                        if index in hit:
-                            hit[index] += [k]
-                        else:
-                            hit[index] = [k]
-
-def process_hit(hit, tickets, df):
-    for k,row in df.iterrows():
-        for i in range(len(tickets)):
-            if tickets[i] in row['Bearish']:
-                if tickets[i] in hit:
-                    hit[tickets[i]] += [k]
-                else:
-                    hit[tickets[i]] = [k]
-def print_hit(df):
-    for i,row in df.iterrows():
-        print('----------------')
-        print(row[0], row[2])
-        for j in range(row[2]):
-            print(row[1][j])
-        print('----------------')    
-
-def process_hits(main_df):
-    df = process_data_corr(main_df, verbose=False)        
-    tickets = get_tickets()
-    hit = {}
-    process_hit_corr(hit, tickets, df)
-    #process_hit(hit, tickets, df)
-                              
-    #with open('hit.pickle', 'wb') as handle:
-    #     pickle.dump(hit, handle)
-    #with open('hit.pickle', 'rb') as handle:
-    #    hit = pickle.load(handle)
-
-    df = pd.DataFrame(hit.items())
-    df[2] = df[1].apply(lambda row : len(row))
-    df = df.sort_values(2, ascending=False)
-    #df[df[2] > 4]
-    #df[df[0] == 'FLRY3 BBSE3']
-    #print_hit(df[df[2] > 6])
-    pdb.set_trace()
 
 def to_timezone(row):
     delta = 0
@@ -547,67 +363,23 @@ def day_trade(tickers):
     
     mpf.show()
 
-def long_short(df, tickets):
-
-
-  def which_level(pct):
-      if isinstance(pct,float):
-          if pct > 0:
-              decimal = pct % 1
-              integer = pct // 1
-              i = 0  
-              for i in range(len(LEVELS)):
-                  if decimal < LEVELS[i]:
-                      i += 1
-                      break
-              return i + integer * len(LEVELS)
-          else:
-              decimal = pct % -1
-              integer = pct // -1
-              i = 0  
-              for i in range(len(LEVELS)):
-                  if decimal > -LEVELS[i]:
-                      i += 1
-                      break
-              return (i + integer * len(LEVELS)) * -1
-      else:
-          return pct
-  dates = df.reset_index()['Data/Hora'].dt.date
-  dates = dates.drop_duplicates()
-  dates = dates.reset_index()
-  dates = dates['Data/Hora']
-  for i,row in dates.items():
-      if i > 0:
-          df1 = df[(df['Ativo'] == tickets[0]) & (df.index.date == dates[i-1])]
-          df2 = df[(df['Ativo'] == tickets[-1]) & (df.index.date == dates[i-1])]
-
-          df3 = df[(df['Ativo'] == tickets[0]) & (df.index.date == row)]
-          df4 = df[(df['Ativo'] == tickets[-1]) & (df.index.date == row)]
-
-          df3 = pd.DataFrame((df3['Último'] - df1['Mínimo'].min()) / (df1['Máximo'].max() - df1['Mínimo'].min()) )
-          df3.rename(columns={'Último': tickets[0]}, inplace=True)  
-          df4 = pd.DataFrame((df4['Último'] - df2['Mínimo'].min()) / (df2['Máximo'].max() - df2['Mínimo'].min()) )
-          df4.rename(columns={'Último': tickets[-1]}, inplace=True)
-
-          df5 = pd.concat([df3,df4], axis=1)
-          df5.reset_index(inplace=True)
-          df5.dropna(inplace=True)
-          print('*******************')  
-          print(df5['Data/Hora'].iloc[-1].strftime('%Y-%m-%d'))
-          print('*******************')
-          for i,row in df5.iterrows():
-              level1 = which_level(round(row[1],3))
-              level2 = which_level(round(row[2],3))
-              print(row[0].strftime('%H:%M'), max(level1, level2) - min(level1, level2) )
-              
-        
+def strategy(main_df):
+    df = main_df[main_df.index.date == main_df.index[-1].date()]
+    for i,ticket in df['Ativo'].drop_duplicates().items():
+        df1 = df[df['Ativo'] == ticket]
+        vol = df1['Financeiro'].sum()
+        df1 = df1[df1['Último'] == df1['Último'].min()]
+        if vol >  10**7:
+            #12' in df1.index.strftime('%H').values and 
+            print(ticket, df1.iloc[0].name.strftime('%H:%M'), format_volume(vol))
+            
 def main(update_tickets=False):
     global count
     #date1 = dt.datetime.now().strftime('%Y-%m-%d')
-    date1 = '2023-12-18' 
+    date1 = '2024-01-10' 
     if not os.path.exists(MAIN_DF_FILE):
-       #tickets = get_tickets()
-       tickets = ['PETR4', 'PETR3', 'BBDC4', 'BBDC3', 'GOAU4', 'GGBR4']
+       tickets = get_tickets()
+       #tickets = ['PETR4', 'PETR3', 'BBDC4', 'BBDC3', 'GOAU4', 'GGBR4']
        df1 = pd.DataFrame({'Ativo' : tickets, 'Data/Hora' : dt.datetime.strptime(date1 + ' 18:00:00', '%Y-%m-%d %H:%M:%S')})
        df1.set_index('Data/Hora', inplace=True)
        df1 = update(df1)
@@ -615,16 +387,16 @@ def main(update_tickets=False):
        df1.to_pickle(MAIN_DF_FILE)
        
     main_df = pd.read_pickle(MAIN_DF_FILE)
-    main_df = main_df[main_df.index >= dt.datetime.strptime(date1, '%Y-%m-%d')]
+    #main_df = main_df[main_df.index <= dt.datetime.strptime(date1, '%Y-%m-%d')]
     if update_tickets:
        main_df = update(main_df)
     main_df.dropna(inplace=True)
-    #verify_trends(main_df)
-    #correlation(main_df)
-    #process_hits(main_df)
+   
+ 
+     
     #plot(['GOAU4','GGBR4'])
-    day_trade(['GOAU4','GGBR4'])
-    #long_short(main_df,['BBDC4', 'BBDC3'])
+    #day_trade(['GOAU4','GGBR4'])
+    strategy(main_df)
     
 def reset(reset_main):
    empty_json = {}
