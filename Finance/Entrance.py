cb2f45b68b78610c0ee817e6a38dc67e6b9341b5
marcelo-alves87 <marceloalves87@gmail.com>
2024-03-27 09:28:51 -0300

    DayTrade

--- Finance/Entrance.py
+++ Finance/Entrance.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pdb
 import pickle
 import winsound
+import os
 
 dict = {}
     
@@ -22,25 +23,26 @@ def check_alert(ticket, df):
         
 
 while (True):
-    df = pd.read_pickle('main_df.pickle')
-    if not 'Preço Teórico' in df.columns:
-        df.reset_index(inplace=True)
-        df = df[['Data/Hora', 'Ativo', 'Último']]
-        if isinstance(df, pd.DataFrame):
-            df.set_index('Data/Hora', inplace=True)
-            df = df[df.index.date == dt.datetime.now().date()]
-            groups = df.groupby([pd.Grouper(freq='5min'), 'Ativo'])\
-                            .agg([('open','first'),('high', 'max'),('low','min'),('close','last')])
-            groups.reset_index('Data/Hora',inplace=True)
-            for name in groups.index.unique():
-                if name != 'IBOV':
-                    df_ticket = groups.loc[name][::-1]
-                    if not df_ticket.empty and isinstance(df_ticket, pd.DataFrame):
-                        df_ticket.set_index('Data/Hora',inplace=True)
-                        df_ticket.sort_index(inplace=True)                        
-                        if len(df_ticket) > 1 and (name not in dict or dict[name] < df_ticket.index[-2]):
-                            dict[name] = df_ticket.index[-2]
-                            check_alert(name, df_ticket['Último'].iloc[-2])
+    if os.path.exists('main_df.pickle'):
+       df = pd.read_pickle('main_df.pickle')
+       if not 'Preço Teórico' in df.columns:
+           df.reset_index(inplace=True)
+           df = df[['Data/Hora', 'Ativo', 'Último']]
+           if isinstance(df, pd.DataFrame):
+               df.set_index('Data/Hora', inplace=True)
+               df = df[df.index.date == dt.datetime.now().date()]
+               groups = df.groupby([pd.Grouper(freq='5min'), 'Ativo'])\
+                               .agg([('open','first'),('high', 'max'),('low','min'),('close','last')])
+               groups.reset_index('Data/Hora',inplace=True)
+               for name in groups.index.unique():
+                   if name != 'IBOV':
+                       df_ticket = groups.loc[name][::-1]
+                       if not df_ticket.empty and isinstance(df_ticket, pd.DataFrame):
+                           df_ticket.set_index('Data/Hora',inplace=True)
+                           df_ticket.sort_index(inplace=True)                        
+                           if len(df_ticket) > 1 and (name not in dict or dict[name] < df_ticket.index[-2]):
+                               dict[name] = df_ticket.index[-2]
+                               check_alert(name, df_ticket['Último'].iloc[-2])
     time.sleep(1)
         
 
