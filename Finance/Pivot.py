d689b9d9eea6aaebafbcf725ea0bc07f0a4a173c
marcelo-alves87 <marceloalves87@gmail.com>
2024-11-28 14:53:48 -0300

    Add files via upload

--- Finance/Pivot.py
+++ Finance/Pivot.py
@@ -21,16 +21,18 @@
 # Resistance (Fibonacci Extension/Retraction)=P+(High−Low)×1.272
 # Support (1.272)=Low−(High−Low)×1.272
 
-HIGH = 96.35
-LOW = 93.88
-CLOSE = 94.5
+DIGITS = 2
+
+HIGH = 99.88
+LOW = 96.56
+CLOSE = 97.06
 
 EXTENSIONS = [ 1.272, 1.414, 1.618, 2.0, 2.618 ]
 RETRACTIONS = [0.236, 0.382, 0.5, 0.618, 0.786 ]
 
 pivot = ( HIGH + LOW + CLOSE ) / 3
 
-print('Pivot Point (P): {}'.format(round(pivot,2)))
+print('Pivot Point (P): {}'.format(round(pivot,DIGITS)))
 
 for i in range(3):
 
@@ -39,20 +41,20 @@ for i in range(3):
         ri = 2*pivot - LOW
         si = 2*pivot - HIGH
         
-        print('Resistance 1 (R1): {}'.format(round(ri,2)))
-        print('Midpoint 1 (M1): {}'.format(round((pivot + ri)/2,2)))
-        print('Support 1 (S1): {}'.format(round(si,2)))
-        print('Midpoint 2 (M2): {}'.format(round((pivot + si)/2,2)))
+        print('Resistance 1 (R1): {}, Difference to Pivot: {}'.format(round(ri,DIGITS),round(ri - pivot,DIGITS)))
+        print('Midpoint 1 (M1): {}'.format(round((pivot + ri)/2,DIGITS)))
+        print('Support 1 (S1): {}, Difference to Pivot: {}'.format(round(si,DIGITS), round(pivot - si,DIGITS)))
+        print('Midpoint 2 (M2): {}'.format(round((pivot + si)/2,DIGITS)))
 
     if i == 2:
 
         rj = pivot + (HIGH - LOW)
         sj = pivot - (HIGH - LOW)
         
-        print('Resistance 2 (R2): {}'.format(round(rj,2)))
-        print('Midpoint 3 (M3): {}'.format(round((ri + rj)/2,2)))
-        print('Support 2 (S2): {}'.format(round(sj,2)))
-        print('Midpoint 4 (M3): {}'.format(round((si + sj)/2,2)))
+        print('Resistance 2 (R2): {}'.format(round(rj,DIGITS)))
+        print('Midpoint 3 (M3): {}'.format(round((ri + rj)/2,DIGITS)))
+        print('Support 2 (S2): {}'.format(round(sj,DIGITS)))
+        print('Midpoint 4 (M3): {}'.format(round((si + sj)/2,DIGITS)))
     
     if i > 2:
 
@@ -62,13 +64,13 @@ for i in range(3):
         rj = HIGH + (i - 1)*(pivot - LOW)
         sj = LOW - (i - 1)*(HIGH - pivot)  
 
-        print('Resistance '+ str(i) +' (R' + str(i) +'): {}'.format(round(rj,2)))
-        print('Midpoint '+ str(i + 2) +' (M' + str(i + 2) +'): {}'.format(round((ri + rj)/2,2)))
-        print('Support '+ str(i) + ' (S' + str(i) +'): {}'.format(round(sj,2)))
-        print('Midpoint '+ str(i + 3) +' (M' + str(i + 3) +'): {}'.format(round((si + sj)/2,2)))
+        print('Resistance '+ str(i) +' (R' + str(i) +'): {}'.format(round(rj,DIGITS)))
+        print('Midpoint '+ str(i + 2) +' (M' + str(i + 2) +'): {}'.format(round((ri + rj)/2,DIGITS)))
+        print('Support '+ str(i) + ' (S' + str(i) +'): {}'.format(round(sj,DIGITS)))
+        print('Midpoint '+ str(i + 3) +' (M' + str(i + 3) +'): {}'.format(round((si + sj)/2,DIGITS)))
 
 ##for i in range(len(EXTENSIONS)):
 ##
-##    print('Fibonacci Resistance '+ str(i) +' (' + str(EXTENSIONS[i]) +'): {}'.format(round(pivot + (HIGH - LOW) * EXTENSIONS[i],2)))
-##    print('Fibonacci  Support '+ str(i) + ' (' + str(RETRACTIONS[i]) +'): {}'.format(round(LOW - (HIGH - LOW) * RETRACTIONS[i],2)))
+##    print('Fibonacci Resistance '+ str(i) +' (' + str(EXTENSIONS[i]) +'): {}'.format(round(pivot + (HIGH - LOW) * EXTENSIONS[i],DIGITS)))
+##    print('Fibonacci  Support '+ str(i) + ' (' + str(RETRACTIONS[i]) +'): {}'.format(round(LOW - (HIGH - LOW) * RETRACTIONS[i],DIGITS)))
 ##    
