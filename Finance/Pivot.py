08716bb47e20c3fc5bd14b2d1d0275a0aa8cfaec
marcelo-alves87 <marceloalves87@gmail.com>
2024-12-11 13:44:44 -0300

    Add files via upload

--- Finance/Pivot.py
+++ Finance/Pivot.py
@@ -1,5 +1,32 @@
 import pandas as pd
 
+def calculate_pivot_points(date, high, low, close):
+    pp = round((high + low + close) / 3, 2)
+    r1 = round(2 * pp - low, 2)
+    s1 = round(2 * pp - high, 2)
+    r2 = round(pp + (high - low), 2)
+    s2 = round(pp - (high - low), 2)
+    m1 = round((pp + r1) / 2, 2)
+    m2 = round((pp + s1) / 2, 2)
+    m3 = round((r1 + r2) / 2, 2)
+    m4 = round((s1 + s2) / 2, 2)
+    distance_pp_r1 = round(abs(pp - r1), 2)
+    distance_pp_s1 = round(abs(pp - s1), 2)
+    return {
+        'Date' : date,
+        'PP': pp,
+        'R1': r1,
+        'S1': s1,
+        'R2': r2,
+        'S2': s2,
+        'M1': m1,
+        'M2': m2,
+        'M3': m3,
+        'M4': m4,
+        'Distance_PP_R1': distance_pp_r1,
+        'Distance_PP_S1': distance_pp_s1
+    }
+
 # Load the dataset
 file_path = 'stock_dfs/SBSP3.csv'  # Replace with your file path
 data = pd.read_csv(file_path)
@@ -20,41 +47,14 @@ pivot_points = []
 
 # Iterate through the data starting from the second day
 for i in range(1, len(data)):
-    # Get the previous day's data
     prev_day = data.iloc[i - 1]
+    pivot_data = calculate_pivot_points(data.iloc[i]['Datetime'].date(), prev_day['High'], prev_day['Low'], prev_day['Close'])    
+    pivot_points.append(pivot_data)
 
-    # Calculate Pivot Points (PP), Resistances (R1, R2), Supports (S1, S2), and middle points
-    pp = round((prev_day['High'] + prev_day['Low'] + prev_day['Close']) / 3, 2)
-    r1 = round(2 * pp - prev_day['Low'], 2)
-    s1 = round(2 * pp - prev_day['High'], 2)
-    r2 = round(pp + (prev_day['High'] - prev_day['Low']), 2)
-    s2 = round(pp - (prev_day['High'] - prev_day['Low']), 2)
-
-    # Middle points
-    m1 = round((pp + r1) / 2, 2)
-    m2 = round((pp + s1) / 2, 2)
-    m3 = round((r1 + r2) / 2, 2)
-    m4 = round((s1 + s2) / 2, 2)
-
-    # Distances
-    distance_pp_r1 = round(abs(pp - r1), 2)
-    distance_pp_s1 = round(abs(pp - s1), 2)
-
-    # Append results to the pivot_points list
-    pivot_points.append({
-        'Date': data.iloc[i]['Datetime'].date(),
-        'PP': pp,
-        'R1': r1,
-        'S1': s1,
-        'R2': r2,
-        'S2': s2,
-        'M1': m1,
-        'M2': m2,
-        'M3': m3,
-        'M4': m4,
-        'Distance_PP_R1': distance_pp_r1,
-        'Distance_PP_S1': distance_pp_s1
-    })
+# Calculate pivot points for the day after the last index
+last_day = data.iloc[-1]
+next_day_pivot = calculate_pivot_points(last_day['Datetime'].date() + pd.Timedelta(days=1), last_day['High'], last_day['Low'], last_day['Close'])
+pivot_points.append(next_day_pivot)
 
 # Convert the pivot points list to a DataFrame
 pivot_points_df = pd.DataFrame(pivot_points)
